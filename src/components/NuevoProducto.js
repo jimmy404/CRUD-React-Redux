99059b5222046638827cf58761a7df7d720486bb
Jimmy <bruno_pier@hotmail.com>
2020-02-11 13:00:24 -0300

    Adding styles in NuevoProducto.js

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 const NuevoProducto = () => {
     return(
-        <h1>Desde NuevoProducto</h1>
+        <div className="row justify-content-center">
+            <div className="col-md-8">
+                <div className="card">
+                    <div className="card-body">
+                        <h2 className="text-center mb-4 font-weight-bold">
+                            Agregar Producto
+                        </h2>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
 
