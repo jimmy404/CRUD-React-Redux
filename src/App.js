5c45c459650794a94a06fdb610967823b9dfe3ed
Jimmy <bruno_pier@hotmail.com>
2020-02-11 12:39:10 -0300

    Adding import BrowserRouter - Deleting Fragment - App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import Header from './components/Header';
+
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
-    <Fragment>
+    <Router>
       <Header/>
-    </Fragment>
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Productos} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
