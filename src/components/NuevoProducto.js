086939b45415ea3e1f65c8973ab838e97817aaa1
Jimmy <bruno_pier@hotmail.com>
2020-02-11 13:04:58 -0300

    Adding styles in form - NuevoProducto.js

--- src/components/NuevoProducto.js
+++ src/components/NuevoProducto.js
@@ -9,6 +9,32 @@ const NuevoProducto = () => {
                         <h2 className="text-center mb-4 font-weight-bold">
                             Agregar Producto
                         </h2>
+                        <form>
+                            <div className="form-group">
+                                <label>Nombre Producto</label>
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre Producto"
+                                    name="nombre"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Precio Producto</label>
+                                <input
+                                    type="number"
+                                    className="form-control"
+                                    placeholder="Precio Producto"
+                                    name="precio"
+                                />
+                            </div>
+                            <button
+                                type="submit"
+                                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                            >
+                                Agregar
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
