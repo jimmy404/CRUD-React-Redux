833da34c816f2802d13b0554db7048b56000324a
Jimmy <bruno_pier@hotmail.com>
2020-02-11 12:47:59 -0300

    Adding import EditarProducto in App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import Productos from './components/Productos';
 import NuevoProducto from './components/NuevoProducto';
+import EditarProducto from './components/EditarProducto';
 
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
@@ -13,6 +14,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Productos} />
           <Route exact path="/productos/nuevo" component={NuevoProducto} />
+          <Route exact path="/productos/editar/:id" component={EditarProducto} />
         </Switch>
       </div>
     </Router>
