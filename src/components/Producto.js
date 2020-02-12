ec9071312f69ec2a2cd77f4624bc255652660798
Jimmy <bruno_pier@hotmail.com>
2020-02-11 22:54:06 -0300

    Adding useHistory&button code - Producto.js

--- src/components/Producto.js
+++ src/components/Producto.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory} from 'react-router-dom';
 import Swal from 'sweetalert2';
 
 //Redux
@@ -11,6 +11,7 @@ const Producto = ({producto}) => {
     const { nombre, precio, id} = producto
 
     const dispatch = useDispatch();
+    const history = useHistory(); // habilita history para redireccion
 
 //confirmar si desea eliminar
 const confirmarEliminarProducto = id => {
@@ -32,12 +33,21 @@ const confirmarEliminarProducto = id => {
     })
 }
 
+    //funcion que redirige de forma progrmada
+    const redireccionarEdicion = producto => {
+        history.push(`/productos/editar/${producto.id}`)
+    }
+
     return (
         <tr>
             <td>{nombre}</td>
             <td><span className="font-weight-bold"> $ {precio} </span></td>
             <td className="acciones">
-                <Link to={`/productos/editar/${id}`} className="btn btn-primary mr-2">Editar</Link>
+                <button
+                    type="button"
+                    onClick={() => redireccionarEdicion(producto)}
+                    className="btn btn-primary mr-2"
+                >Editar</button>
                 <button
                     type="button"
                     className="btn btn-danger"
